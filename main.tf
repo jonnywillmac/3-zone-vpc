9b6f44be63610a2902c33975454d99f3a9e6aac4
Jonathan McCormick <jonathan@ssmj.co.uk>
2020-01-31 18:31:59 +0000

    Giving up soon

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "vpc1" {
     ibm = "ibm.us"
   }
   
-   vpc2_ipv4_cidr_block = ${module.vpc2.zone2subnet1}
-   vpc2_peer_ip = ${module.vpc2.zone2vpnip}
+   vpc2_ipv4_cidr_block = "${module.vpc2.zone2subnet1}"
+   vpc2_peer_ip = "${module.vpc2.zone2vpnip}"
 }
 
 module "vpc2" {
@@ -14,6 +14,6 @@ module "vpc2" {
     ibm = "ibm.de"
   }
   
-  vpc1_ipv4_cidr_block = ${module.vpc1.zone1subnet1}
-  vpc1_peer_ip = ${module.vpc1.zone1vpnip}
+  vpc1_ipv4_cidr_block = "${module.vpc1.zone1subnet1}"
+  vpc1_peer_ip = "${module.vpc1.zone1vpnip}"
 }
